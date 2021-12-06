7f94bfa11cf2f528c2d88464a03bfdcad05c7de1
crowwofficial <94466321+crowwofficial@users.noreply.github.com>
2021-12-06 10:04:42 +0700

    Update infobot.js

--- shop/infobot.js
+++ shop/infobot.js
@@ -23,13 +23,13 @@ return `ℒ⃝👾 *B O T  S T A T I S T I C*
 ℒ⃝👨‍💻 *C O N N E C T  W I T H  M E*
 
 ⛎ Api : api-alphabot.herokuapp.com
-♈ YouTube : youtube.com/c/zeeoneofc
+♈ YouTube : https://youtube.com/CROWW OFFICIAL
 ♉ Github : zeeoneofc.github.io
-♊ Instagram : instagram.com/zeeoneofc
-♋ Tiktok : tiktok.com/@zeeoneofc
-♌ Telegram : t.me/zeeoneofc
-♎ Ch Telegram : t.me/zeeone_ofc
-♏ WhatsApp : bit.ly/zeeone_ofc
+♊ Instagram : instagram.com/tahukuyang
+♋ Tiktok : tiktok.com/@ikyy_ganzz913
+♌ Telegram : t.me/@kyy1205
+♎ Ch Telegram : not available
+♏ WhatsApp : wa.me/6283870799812
 `}
 
 exports.infobot = infobot
