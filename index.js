799355722800bafb6d88ddee9e9ad6ae1900bbe9
crowwofficial <94466321+crowwofficial@users.noreply.github.com>
2021-12-06 10:49:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ autoregister = setting.user_register
 
 img = setting.img
 baper = 'SUB YT CROWW OFFICIAL 🔰'
-apiku = 'https://youtu.be/2rWbjrDIAyA'
+apiku = 'https://youtube.com/channel/UCWi1qCfMWXfKQSqrttFMWMw'
 gc_wa_lu = 'https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV' //klo gk punya gc wa gk usah di ganti 👍
 targetpc = setting.ownerNumberr
 owner = targetpc
